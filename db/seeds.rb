dab6af928f858bcddc228da085af9ebc9ee1d01c
masi <masi@fozzy.com>
2024-05-17 16:18:38 +0300

    fixed slim lint add seed fixed pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+users = []
+STATES = Bulletin.aasm.states.map(&:name)
+
+5.times do
+  users << User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
+
+3.times do
+  category = Category.find_or_create_by(name: Faker::ProgrammingLanguage.name)
+  bulletin_image = ['first.jpg', 'second.jpg', 'third.jpg'].map do |file_path|
+    Rails.root.join("test/fixtures/files/#{file_path}")
+  end
+
+  20.times do
+    bulletin = users.sample.bulletins.build(
+      description: Faker::Lorem.paragraph,
+      title: Faker::Lorem.sentence(word_count: 3),
+      category:,
+      state: STATES.sample
+    )
+    bulletin.image.attach(io: File.open(bulletin_image.sample), filename: 'filename.jpg')
+    bulletin.save
+    sleep 1
+  end
+end
